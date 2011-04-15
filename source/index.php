4351a8bf867becab7be60e9d09e9a672fb481db9
Freeky <martin.pesl@gmail.com>
2011-04-15 14:26:40 +0200

    Overview Template erstellt

---
+++ source/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Zeiterfassung</title>
+<link href="./css/screen.css" media="screen" rel="stylesheet"
+	type="text/css" />
+</head>
+<body>
+	<div class="column span-3">Test</div>
+	<div class="column span-3">Some Test content</div>
+</body>
+</html>
---
+++ source/overview.php
@@ -0,0 +1,84 @@
+<?php
+?>
+<html>
+<head>
+<link href="./css/screen.css" media="screen" rel="stylesheet"
+	type="text/css" />
+</head>
+
+<body>
+	<div class="column span-5">
+		<h1>Filter</h1>
+		<br />
+		<h2>Filter</h2>
+		<br />
+		<h3>Filter</h3>
+		<br />
+		<h4>Filter</h4>
+		<br />
+	</div>
+	<div class="column">
+		<table>
+			<tr>
+				<td width="30">ID</td>
+				<td width="100">Datum</td>
+				<td width="200">Auftraggeber</td>
+				<td width="300">Beschreibung</td>
+				<td width="100">Status</td>
+				<td width="50">Zeit</td>
+			</tr>
+			<form method="POST">
+				<tr>
+					<td>!icon!</td>
+					<td><input name="date" type="text" style="width: 100%"></td>
+					<td><input name="date" type="text" style="width: 100%"></td>
+					<td><input name="date" type="text" style="width: 100%"></td>
+					<td><input name="date" type="text" style="width: 100%"></td>
+					<td><input name="date" type="text" style="width: 100%"></td>
+				</tr>
+			</form>
+			<tr>
+				<td>5</td>
+				<td>19-02-2011</td>
+				<td>Hochschule</td>
+				<td>Doing some REALY crazy shit</td>
+				<td>DONE</td>
+				<td>2.5</td>
+			</tr>
+			<tr class="time_entry"
+				onclick="window.location.href = 'http://www.google.de'">
+				<td>4</td>
+				<td>18-02-2011</td>
+				<td>Hochschule</td>
+				<td>Doing some REALY crazy shit</td>
+				<td>DONE</td>
+				<td>2.5</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>18-02-2011</td>
+				<td>Google</td>
+				<td>Doing some REALY crazy shit</td>
+				<td>DONE</td>
+				<td>2.5</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>16-02-2011</td>
+				<td>Hochschule</td>
+				<td>Doing some REALY crazy shit</td>
+				<td>DONE</td>
+				<td>2.5</td>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>11-02-2011</td>
+				<td>Hochschule</td>
+				<td>Doing some REALY crazy shit</td>
+				<td>DONE</td>
+				<td>2.5</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
