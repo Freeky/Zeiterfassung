5ce5bc468697d38e2eda52504d79838b324b881e
robman1990 <2funny4u@gmx.de>
2011-05-27 12:36:02 +0200

    reqire_once_fixed

--- source/public/admin/index.php
+++ source/public/admin/index.php
@@ -1,5 +1,5 @@
 <?php
-	require_once(dirname(__FILE__) . "../../controls/admin/Controller.php");
+	require_once(dirname(__FILE__) . "\..\..\controls\admin\Controller.php");
 	
 	$controller = new Controller();
 	
